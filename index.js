68a459e597b98e33df95551fcc9fa0581117c0ee
André Luis Domingos Pereira <andrerj2000@gmail.com>
2024-02-01 10:18:19 -0300

    New features

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
+/*Para que o prompt-sync funcione voce precisará instalar os seguintes npm:
+  Open terminal and run:-
+  Step 1: npm init
+  Step 2: npm install prompt-sync*/
 
-//var name = console.log(prompt("Entrada: "));
+//Declarando as variaveis
+let nameUserOfTheGame;
+let scoreLarge;
+let scoreSmall;
+//let scoreGeral;
+
+//Declaração para usar o promt
 const prompt = require("prompt-sync")();
 
-let a = prompt("enter a number: ")
-console.log(a)
+//Criando função para entrada de dados pelo usuario:
+
+function enter(){
+    nameUserOfTheGame = prompt("Entre com o nome: ");
+    scoreLarge = prompt("Entre com o maior valor do Score: ");
+    scoreSmall = prompt("Entre com o menor valor do Score: ");
+
+}
+
+function CalculateScoreGeral(){
+    return scoreLarge - scoreSmall;
+    
+}
+
+function listAllOfUsers(){
+    console.log(`O jogador: ${nameUserOfTheGame} esta com o score: ${CalculateScoreGeral()}`);
+}
+
+//Executando funções
+enter();
+listAllOfUsers();
+
